29352ff0656f093093ae3e3957abb93c60d70a66
marquis-codes <abahmarquis@gmail.com>
2026-03-05 23:56:53 +0100

    fix code

--- .output/public/_nuxt/BQzjcmcN.js
+++
@@ -1,1 +0,0 @@
-import{a as e}from"./C41C8tDG.js";import{O as o,z as r}from"./CbmHTv_N.js";const u=o((t,s)=>{const{token:a}=e();if(!a.value&&t.path.startsWith("/admin"))return r("/login")});export{u as default};
---
+++ .output/public/_nuxt/CoJgW_JM.js
@@ -0,0 +1,1 @@
+import{G as r}from"./C4TP1Ha2.js";const o={register:t=>r.post("/auth/register",t),login:t=>r.post("/auth/login",t),getProfile:()=>r.get("/auth/profile")};export{o as a};
